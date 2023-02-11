05bbfe2a03c2cf6c34f34f48fd74a724b4669080
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-02-11 22:39:13 +0700

    Update gt.cpp

--- proxy/gt.cpp
+++ proxy/gt.cpp
@@ -3,7 +3,7 @@
 #include "server.h"
 #include "utils.h"
 
-std::string gt::version = "3.96";
+std::string gt::version = "4.15";
 std::string gt::flag = "id";
 bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
